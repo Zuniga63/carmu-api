35bc83b39ea08ba23a72a89334b56dc0c10b38e4
Andres Zuñiga <andres.zuniga.063@gmail.com>
2022-08-16 10:23:57 -0500

    add custom errors class

---
+++ src/utils/sendError.ts
@@ -0,0 +1,31 @@
+import { Response } from 'express';
+import ResponseInfo from './ResponseInfo';
+
+export default function sendError(error: any, res: Response) {
+  const { name: errorName, message }: { name: string; message: string } = error;
+  let code: number = 500;
+  const info = new ResponseInfo(message);
+
+  switch (errorName) {
+    case 'InvalidSignInError':
+      code = 400;
+      break;
+    case 'ValidationError':
+      info.message = 'Error de validación';
+      info.validationErrors = error.errors;
+      code = 400;
+      break;
+    case 'AuthError':
+      code = 401;
+      break;
+    case 'NotFoundError':
+      code = 404;
+      break;
+    default:
+      // eslint-disable-next-line no-console
+      console.log(error);
+      break;
+  }
+
+  res.status(code).json(info);
+}
