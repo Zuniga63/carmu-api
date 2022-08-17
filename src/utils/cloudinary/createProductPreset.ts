64b55fa5fb0fc6fc21d05114f9e73785d43b1adc
Andres Zuñiga <andres.zuniga.063@gmail.com>
2022-08-16 20:01:15 -0500

    add script for create cloudinary presets

---
+++ src/utils/cloudinary/createProductPreset.ts
@@ -0,0 +1,22 @@
+import { v2 as cloudinary } from 'cloudinary';
+
+export default async function createProductPreset(): Promise<void> {
+  try {
+    const preset = await cloudinary.api.create_upload_preset({
+      name: 'carmu_product_preset',
+      folder: 'carmu/products',
+      resource_type: 'image',
+      allowed_formats: 'jpg, png, gif, webp, bmp, jpe, jpeg',
+      access_mode: 'public',
+      unique_filename: true,
+      auto_tagging: 0.7,
+      overwrite: true,
+
+      transformation: [{ width: 1080, crop: 'scale', dpr: 'auto', quality: 'auto:best' }],
+    });
+
+    console.log(preset);
+  } catch (error: any) {
+    console.error(error.error.message);
+  }
+}
