8f917064f2955688219aec9da7045001bfad6db1
Andres Zuñiga <andres.zuniga.063@gmail.com>
2022-08-17 20:08:27 -0500

    add category crud in the index

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,8 +1,10 @@
 import { Router } from 'express';
 import AuthRouter from './Auth.router';
+import CategoryRouter from './category.router';
 
 const rootRouter = Router();
 
 rootRouter.use('/auth', AuthRouter);
+rootRouter.use('/categories', CategoryRouter);
 
 export default rootRouter;
