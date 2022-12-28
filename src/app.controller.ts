2f7f693ed48c5c76c121cb51ca52502bf2ad105e
Andres Zuñiga <andres.zuniga.063@gmail.com>
2022-12-28 15:42:22 -0500

    feat: add end point for install cloudinary presets

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,5 +1,14 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post, UseGuards } from '@nestjs/common';
+import {
+  ApiBearerAuth,
+  ApiForbiddenResponse,
+  ApiUnauthorizedResponse,
+} from '@nestjs/swagger';
 import { AppService } from './app.service';
+import { RequirePermissions } from './modules/auth/decorators/required-permissions.decorator';
+import { JwtAuthGuard } from './modules/auth/guards/jwt-auth.guard';
+import { PermissionsGuards } from './modules/auth/guards/permissions.guard';
+import { Permission } from './modules/auth/permission.enum';
 
 @Controller()
 export class AppController {
@@ -8,5 +17,19 @@ export class AppController {
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Post('/config/install-cloudinary-presets')
+  @UseGuards(JwtAuthGuard, PermissionsGuards)
+  @RequirePermissions(Permission.INSTALL_CLOUD_PRESETS)
+  @ApiBearerAuth()
+  @ApiUnauthorizedResponse({
+    description: 'Only admin can use this end point.',
+  })
+  @ApiForbiddenResponse({
+    description: 'Only super admin can acces to this end point',
+  })
+  installPresets(): void {
+    this.appService.installPressets();
   }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,10 +1,19 @@
 import { Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
+import { CloudinaryService } from './modules/cloudinary/cloudinary.service';
 
 @Injectable()
 export class AppService {
-  constructor(private configService: ConfigService) {}
+  constructor(
+    private configService: ConfigService,
+    private cloudinaryService: CloudinaryService
+  ) {}
+
   getHello(): string {
     return `Welcome to ${this.configService.get<string>('APP_NAME')}`;
+  }
+
+  installPressets(): void {
+    this.cloudinaryService.createPresets();
   }
 }
