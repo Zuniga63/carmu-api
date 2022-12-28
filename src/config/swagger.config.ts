dae43bec58268a6eda0227d4f1d6baa78a3c5ee2
Andres Zuñiga <andres.zuniga.063@gmail.com>
2022-12-28 15:05:37 -0500

    feat: add app name from env file

--- src/config/swagger.config.ts
+++ src/config/swagger.config.ts
@@ -1,8 +1,8 @@
 import { DocumentBuilder } from '@nestjs/swagger';
 
 export default new DocumentBuilder()
-  .setTitle('Nest Auth Template')
-  .setDescription('This is an API for managment the auth users')
+  .setTitle(`${process.env.APP_NAME}`)
+  .setDescription('This is an API for managment the carmú store')
   .setVersion('1.0')
   .addTag('Auth', 'End point for register and login')
   .addTag('Users', 'Route for user administration')
