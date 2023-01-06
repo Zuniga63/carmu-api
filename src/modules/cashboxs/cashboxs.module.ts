70551d3e3c7a76a8f56c906e65b71bdb52079d98
Andres Zuñiga <andres.zuniga.063@gmail.com>
2023-01-05 19:21:18 -0500

    feat: add mongoose modules

--- src/modules/cashboxs/cashboxs.module.ts
+++ src/modules/cashboxs/cashboxs.module.ts
@@ -1,8 +1,25 @@
 import { Module } from '@nestjs/common';
 import { CashboxsService } from './cashboxs.service';
 import { CashboxsController } from './cashboxs.controller';
+import { MongooseModule } from '@nestjs/mongoose';
+import { Cashbox, CashboxSchema } from './schemas/cashbox.schema';
+import {
+  CashboxTransaction,
+  CashboxTransactionSchema,
+} from './schemas/cashbox-transaction.schema';
+import {
+  CashClosingRecord,
+  CashClosingRecordSchema,
+} from './schemas/cash-closing-record.schema';
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([
+      { name: Cashbox.name, schema: CashboxSchema },
+      { name: CashboxTransaction.name, schema: CashboxTransactionSchema },
+      { name: CashClosingRecord.name, schema: CashClosingRecordSchema },
+    ]),
+  ],
   controllers: [CashboxsController],
   providers: [CashboxsService],
 })
