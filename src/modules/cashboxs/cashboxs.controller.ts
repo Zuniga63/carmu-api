b87db50107ad3559ed8cb9c4bc1e70efcc804a2b
Andres Zuñiga <andres.zuniga.063@gmail.com>
2023-01-11 13:33:32 -0500

    chore: update the ids types

--- src/modules/cashboxs/cashboxs.controller.ts
+++ src/modules/cashboxs/cashboxs.controller.ts
@@ -6,13 +6,20 @@ import {
   Patch,
   Param,
   Delete,
+  UseGuards,
+  Req,
 } from '@nestjs/common';
 import { ApiBearerAuth, ApiForbiddenResponse, ApiTags } from '@nestjs/swagger';
+import { Request } from 'express';
+import { JwtAuthGuard } from '../auth/guards/jwt-auth.guard';
+import { PermissionsGuards } from '../auth/guards/permissions.guard';
+import { User } from '../users/schema/user.schema';
 import { CashboxsService } from './cashboxs.service';
 import { CreateCashboxDto } from './dto/create-cashbox.dto';
 import { UpdateCashboxDto } from './dto/update-cashbox.dto';
 
 @Controller('cashboxs')
+@UseGuards(JwtAuthGuard, PermissionsGuards)
 @ApiTags('Cashboxs')
 @ApiBearerAuth()
 @ApiForbiddenResponse({
@@ -22,23 +29,23 @@ export class CashboxsController {
   constructor(private readonly cashboxsService: CashboxsService) {}
 
   @Post()
-  create(@Body() createCashboxDto: CreateCashboxDto) {
-    return this.cashboxsService.create(createCashboxDto);
+  create(@Body() createCashboxDto: CreateCashboxDto, @Req() req: Request) {
+    return this.cashboxsService.create(createCashboxDto, req.user as User);
   }
 
   @Get()
-  findAll() {
-    return this.cashboxsService.findAll();
+  findAll(@Req() req: Request) {
+    return this.cashboxsService.findAll(req.user as User);
   }
 
   @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.cashboxsService.findOne(+id);
+  findOne(@Param('id') id: string, @Req() req: Request) {
+    return this.cashboxsService.findOne(id, req.user as User);
   }
 
   @Patch(':id')
   update(@Param('id') id: string, @Body() updateCashboxDto: UpdateCashboxDto) {
-    return this.cashboxsService.update(+id, updateCashboxDto);
+    return this.cashboxsService.update(id, updateCashboxDto);
   }
 
   @Delete(':id')
