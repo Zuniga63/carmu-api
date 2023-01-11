001e64566c1c706c5ed8034fd255f9dd2de11750
Andres Zuñiga <andres.zuniga.063@gmail.com>
2023-01-11 13:31:02 -0500

    feat: add Login user type

--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -11,6 +11,8 @@ import { CreateUserDto } from '../users/dto/create-user.dto';
 import { ChangePasswordDto } from './dto/change-password.tdo';
 import { MailService } from '../mail/mail.service';
 
+type UserLoging = Omit<User, 'password'>;
+
 @Injectable()
 export class AuthService {
   constructor(
@@ -22,7 +24,7 @@ export class AuthService {
   async validateUser(
     username: string,
     pass: string
-  ): Promise<Omit<User, 'password'> | null> {
+  ): Promise<UserLoging | null> {
     const user = await this.usersService.findOneByEmail(username);
 
     if (user && user.isActive && compareSync(pass, user.password)) {
@@ -33,16 +35,16 @@ export class AuthService {
     return null;
   }
 
-  protected createAccessToken(user: Omit<User, 'password'>) {
+  protected createAccessToken(user: UserLoging) {
     const payload = { username: user.email, sub: user.id };
     return this.jwtService.sign(payload);
   }
 
-  async login(user: Omit<User, 'password'>) {
+  async login(user: UserLoging) {
     return { user, access_token: this.createAccessToken(user) };
   }
 
-  async sendConfirmEmail(user: Omit<User, 'password'>) {
+  async sendConfirmEmail(user: UserLoging) {
     const token = Math.floor(1000 + Math.random() * 9000).toString();
     await this.mailService.sendUserConfirmation(user, token);
   }
@@ -54,10 +56,7 @@ export class AuthService {
     return this.login(user);
   }
 
-  async updateProfilePhoto(
-    user: Omit<User, 'password'>,
-    file: Express.Multer.File
-  ) {
+  async updateProfilePhoto(user: UserLoging, file: Express.Multer.File) {
     const userUpdated = await this.usersService.updateProfilePhoto(
       user.id,
       file
@@ -65,14 +64,14 @@ export class AuthService {
     return { user: userUpdated };
   }
 
-  async removeProfilePhoto(user: Omit<User, 'password'>) {
+  async removeProfilePhoto(user: UserLoging) {
     const userUpdated = await this.usersService.removeProfilePhoto(user.id);
 
     return { user: userUpdated };
   }
 
   async changeProfilePassword(
-    authUser: Omit<User, 'password'>,
+    authUser: UserLoging,
     changePasswordDto: ChangePasswordDto
   ) {
     const user = await this.usersService.findOneByEmail(authUser.email);
