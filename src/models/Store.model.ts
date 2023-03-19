87d2abf5ed1c74e5a420796a0d011c92cc7c0cb7
Andres Zuñiga <andres.zuniga.063@gmail.com>
2023-03-18 21:30:26 -0500

    feat: add bussiness info, controller and routes

---
+++ src/models/Store.model.ts
@@ -0,0 +1,26 @@
+import { HydratedDocument, Schema, Types, Model, model } from 'mongoose';
+
+export interface IStoreModel {
+  name: string;
+  address?: string;
+  phone?: string;
+  defaultBox?: Types.ObjectId;
+  invoices: Types.ObjectId[];
+}
+
+export type StoreDocument = HydratedDocument<IStoreModel>;
+
+const scheme = new Schema<IStoreModel, Model<IStoreModel>>({
+  name: {
+    type: String,
+    minlength: [3, 'Nombre demasiado corto.'],
+    maxlength: [90, 'Nombre de usuario muy largo.'],
+    required: [true, 'Nombre del local es requerido'],
+  },
+  address: String,
+  phone: String,
+  defaultBox: { type: Schema.Types.ObjectId, ref: 'Cashbox' },
+  invoices: [{ type: Schema.Types.ObjectId, ref: 'Invoice' }],
+});
+
+export default model<IStoreModel>('Store', scheme);
