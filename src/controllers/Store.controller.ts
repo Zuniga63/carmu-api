6d8df38625874d51614e3652ccbf4472d70aa83f
Andres Zuñiga <andres.zuniga.063@gmail.com>
2023-04-18 16:09:24 -0500

    feat: add weekly and monthly sales

--- src/controllers/Store.controller.ts
+++ src/controllers/Store.controller.ts
@@ -1,11 +1,36 @@
+import dayjs from 'dayjs';
 import { Request, Response } from 'express';
+import InvoiceModel from 'src/models/Invoice.model';
 import StoreModel from 'src/models/Store.model';
 import NotFoundError from 'src/utils/errors/NotFoundError';
 import sendError from 'src/utils/sendError';
 
 export async function list(_req: Request, res: Response) {
   try {
-    const stores = await StoreModel.find({}).sort('name').populate('defaultBox');
+    const storeDocuments = await StoreModel.find({}).sort('name').populate('defaultBox');
+    const groupQuery = { _id: '$premiseStore', amount: { $sum: '$amount' } };
+
+    const [monthlySales, weeklySales] = await Promise.all([
+      InvoiceModel.aggregate()
+        .sort('expeditionDate')
+        .match({ expeditionDate: { $gte: dayjs().subtract(1, 'month').toDate() }, cancel: false })
+        .group(groupQuery),
+      InvoiceModel.aggregate()
+        .sort('expeditionDate')
+        .match({ expeditionDate: { $gte: dayjs().startOf('week').toDate() }, cancel: false })
+        .group(groupQuery),
+    ]);
+
+    const stores = storeDocuments.map((document) => {
+      const monthlySale = monthlySales.find((item) => document._id.equals(item._id));
+      const weeklySale = weeklySales.find((item) => document._id.equals(item._id));
+      const store = document.toObject();
+      store.monthlySales = monthlySale ? monthlySale.amount : 0;
+      store.weeklySales = weeklySale ? weeklySale.amount : 0;
+
+      return store;
+    });
+
     res.status(200).json(stores);
   } catch (error) {
     sendError(error, res);
@@ -40,6 +65,21 @@ export async function update(req: Request, res: Response) {
     await commercialPremise.populate('defaultBox');
 
     res.status(200).json(commercialPremise);
+  } catch (error) {
+    sendError(error, res);
+  }
+}
+
+export async function premiseStoreInvoices(_req: Request, res: Response) {
+  try {
+    const stores = await StoreModel.find({}).populate({
+      path: 'invoices',
+      match: { cancel: false },
+      options: { sort: { expeditionDate: 1 } },
+      select: 'expeditionDate amount cancel',
+    });
+
+    res.status(200).json(stores);
   } catch (error) {
     sendError(error, res);
   }
